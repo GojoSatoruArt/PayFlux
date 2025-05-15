48fd5f55cbac0e4c401c5d62745ee4b5e985308a
GojoSatoruArt <gsatoru.art@gmail.com>
2025-05-15 15:32:55 +0800

    added signin UI

--- src/lib/components/section.svelte
+++ src/lib/components/section.svelte
@@ -3,6 +3,6 @@
 </script>
 
 
-<section class="section overflow-x-hidden w-full  h-dvh py-(--spacing-gap) mb-0">
+<section class="section overflow-x-hidden w-full auto md:h-dvh py-25 px-(--spacing-gap) md:py-(--spacing-gap) mb-0">
 <slot />
     </section>
