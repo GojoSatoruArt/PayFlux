506d55ec59181016eea4314615aa7bb127a0a567
GojoSatoruArt <gsatoru.art@gmail.com>
2025-05-16 17:37:59 +0800

    add profileswitch ui and animations

--- src/lib/components/modalbg.svelte
+++ src/lib/components/modalbg.svelte
@@ -3,7 +3,7 @@ import { loginShow } from "./script/popup.svelte";
 </script>
 
 
-<div on:click={$loginShow = false} class="modal-bg z-100 fixed p-10 flex justify-center items-start w-full h-full bg-(--color-modal-bg) backdrop-blur-[1px]">
+<div class="modal-bg z-100 fixed p-10 flex justify-center items-start w-full h-full bg-(--color-modal-bg) backdrop-blur-[1px]">
     <slot/>
 
 </div>
